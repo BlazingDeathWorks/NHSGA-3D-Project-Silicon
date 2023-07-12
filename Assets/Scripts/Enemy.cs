0d75702e256a79fa8b9107e39ed1978581bc1ecf
Future Inspire Games <shunsei.chan@gmail.com>
2023-07-12 14:51:03 -0400

    Enemy AI Completed

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,17 +5,38 @@ using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    public Transform destination;
-    [SerializeField] private NavMeshAgent agent;
-    // Start is called before the first frame update
-    void Start()
+    private Transform destination;
+    private NavMeshAgent agent;
+
+
+    private void Awake()
     {
-        agent.SetDestination(destination.position);
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (destination == null) return;
+
+        agent.SetDestination(destination.position);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            agent.isStopped = false;
+            destination = other.transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            destination = null;
+            agent.isStopped = true;
+        }
     }
 }
