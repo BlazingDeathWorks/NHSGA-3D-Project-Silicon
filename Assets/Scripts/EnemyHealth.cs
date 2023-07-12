d866306a4c0272c6654e879cdd65fba2c88061b0
Future Inspire Games <shunsei.chan@gmail.com>
2023-07-12 15:15:40 -0400

    Enemy Health Completed and Other Asset Imports

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
 {
-    private void OnParticleCollision(GameObject other)
+    [SerializeField] private int health = 3;
+
+    //Make sure the bullet is NOT a trigger
+    private void OnCollisionEnter(Collision other)
     {
-        Destroy(gameObject);
+        if (!other.gameObject.CompareTag("Bullet")) return;
+        health--;
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
