ad2b7a8ef0d88be1f9ad3f0a68bf037a47d1825f
Future Inspire Games <shunsei.chan@gmail.com>
2023-07-12 21:38:30 -0400

    Player Health UI Completed

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] private int health = 5;
     private Vector3 originalPosition;
+    [SerializeField] private Slider slider;
 
     private void Awake()
     {
         originalPosition = transform.position;
+        slider.maxValue = health;
+        slider.value = health;
     }
 
     public void RepositionPlayer()
@@ -20,6 +24,7 @@ public class PlayerHealth : MonoBehaviour
     public void TakeDamage()
     {
         health--;
+        slider.value = health;
         if (health <= 0)
         {
             Destroy(gameObject);
